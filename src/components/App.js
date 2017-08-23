b17ad48d93495fe5cd9b123488889f2b9f613cdf
SuperMan0719 <buper0719@gmail.com>
2017-08-23 18:59:24 +0300

    Add payment window from backend. Various style adjustments.

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 
 import Main from '../components/Main';
 import Team from '../components/Team';
-import RecentOrders from '../components/RecentOrders';
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
-        <RecentOrders />
         <Team />
       </div>
     );
