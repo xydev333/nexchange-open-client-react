ffb1d8cc8f451af5bdf0e40e41481dc6a7b9a17c
SuperMan0719 <buper0719@gmail.com>
2017-08-24 19:06:58 +0300

    Hot fix of exchange widget

--- src/components/ExchangeWidget.js
+++ src/components/ExchangeWidget.js
@@ -27,7 +27,9 @@ class ExchangeWidget extends Component {
 	}
 
 	componentDidMount() {
-		this.updatePrices();
+		setTimeout(() => {
+			this.updatePrices();
+		}, 100);
 	}
 
 	componentWillUnmount() {
@@ -35,7 +37,7 @@ class ExchangeWidget extends Component {
 	}
 
 	updatePrices() {
-		this.props.fetchPrice({pair: `${this.props.selectedCoin.present.deposit}${this.props.selectedCoin.present.receive}`, lastEdited: 'deposit', amount: this.props.amounts.deposit});
+		this.props.fetchPrice({pair: `${this.props.selectedCoin.receive}${this.props.selectedCoin.deposit}`, lastEdited: 'deposit', amount: this.props.amounts.deposit});
 
 		this.timeout = setTimeout(() => {
 			this.updatePrices();
@@ -53,7 +55,7 @@ class ExchangeWidget extends Component {
 				"amount_base": this.props.amounts.receive,
 				"is_default_rule": true,
 				"pair": {
-					"name": `${this.props.selectedCoin.present.receive}${this.props.selectedCoin.present.deposit}`
+					"name": `${this.props.selectedCoin.receive}${this.props.selectedCoin.deposit}`
 				},
 				"withdraw_address": {
 					"address": this.props.wallet.address,
