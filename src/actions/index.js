066b6da89c687ceb9ad96e2bf557dd0755aac0c3
SuperMan0719 <buper0719@gmail.com>
2017-08-28 12:29:05 +0300

    Adjust min ETH amount.

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,6 +18,14 @@ export function setWallet(payload) {
 }
 
 export function selectCoin(payload) {
+	// DISPATCH COIN_SELECTED AND
+	// THEN UPDATE AMOUNTS
+	// ACTUALLY, WRONG PLACE, SHOULD
+	// DETECT SELECTED COIN PROPS CHANGE AND THEN
+	// UPDATE AMOUNTS
+	// return (dispatch, getState) {
+	// }
+
 	return {
 		type: 'COIN_SELECTED',
 		payload: payload
@@ -29,36 +37,6 @@ export function updateAmounts(payload) {
 		type: 'UPDATE_AMOUNTS',
 		payload: payload
 	}
-}
-
-export function fetchCoinDetails(payload) {
-	const url = `${config.API_BASE_URL}/currency/`;
-	const request = axios.get(url);
-
-    return (dispatch, getState) => {
-        request
-	        .then(response => {
-	        	if (!response.data.length) return;
-
-	        	console.log(response.data);
-
-	        	// dispatch({type: 'PRICE_FETCHED', payload: {
-	        	// 	price: response.data[0].ticker.ask,
-	        	// 	pair: payload.pair,
-	        	// 	lastFetched: new moment()
-	        	// }});
-
-	        	// if (payload.amount) {
-		        // 	dispatch({type: 'UPDATE_AMOUNTS', payload: {
-		        // 		price: response.data[0].ticker.ask,
-		        // 		amount: payload.amount,
-		        // 		lastEdited: payload.lastEdited,
-		        // 	}});
-	        	// }
-	        }).catch(error => {
-	        	console.log(error);
-	        });    
-    };
 }
 
 export function fetchPrice(payload) {
