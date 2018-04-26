fe4cc09bedeed62f0f8e2a84aced4f1c02cbefeb
SuperMan0719 <buper0719@gmail.com>
2018-04-26 11:25:22 +0300

    Add ICO Landing page submodule.

--- src/helpers/setAuthToken.js
+++ src/helpers/setAuthToken.js
@@ -3,11 +3,7 @@ import config from '../config';
 
 export default () => {
 	axios.interceptors.request.use(function (requestConfig) {
-		// Setting authorization header for /kyc endpoint for some
-		// reason breaks it.
-		if (requestConfig.url.indexOf(config.API_BASE_URL) > -1
-			&& requestConfig.url.indexOf('kyc') === -1
-			&& localStorage.token) {
+		if (requestConfig.url.indexOf(config.API_BASE_URL) > -1 && localStorage.token) {
 			const token = localStorage.token;
 			requestConfig.headers['Authorization'] = `Bearer ${token}`;
 		}
