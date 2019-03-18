047922d0842eb40ebad4032bf523f6085b932ce7
SuperMan0719 <buper0719@gmail.com>
2019-03-18 17:58:58 +0200

    feature/hotjar

--- src/components/Home/Hero/ExchangeWidget/CoinInput/CoinInput.js
+++ src/components/Home/Hero/ExchangeWidget/CoinInput/CoinInput.js
@@ -38,11 +38,6 @@ class CoinInput extends PureComponent {
     ga('send', 'event', 'Order', 'change amount');
   };
 
-  assignMinMaxValue = value => {
-    const simulatedEvent ={target: {value: value.toString()}};
-    this.handleChange(simulatedEvent);
-  };
-
   handleSubmit = event => {
     event.preventDefault();
     this.props.onSubmit();
@@ -109,7 +104,6 @@ class CoinInput extends PureComponent {
               min={this.props.type === 'deposit' ? this.props.price.min_amount_quote : this.props.price.min_amount_base}
               max={this.props.type === 'deposit' ? this.props.price.max_amount_quote : this.props.price.max_amount_base}
               amount={this.props.type === 'deposit' ? this.props.price.deposit : this.props.price.receive}
-              assignMinMaxValue={this.assignMinMaxValue}
             />
           </div>
         )}
--- src/components/MinMax/MinMax.js
+++ src/components/MinMax/MinMax.js
@@ -6,22 +6,15 @@ import styles from './MinMax.scss';
 const MinMax = props => {
   if (props.order && isFiatOrder(props.order) && props.type === 'Deposit') return null;
 
-  let clickable = false;
-  if(props.assignMinMaxValue){
-    clickable = true;
-  }
-
   return (
     <I18n ns="translations">
       {t => (
         <div className={`${styles.minmax} ${props.home ? styles.home : ''}`}>
-          <p className={`${props.min > props.amount && props.amount !== '...' ? 'error' : ''} ${clickable ? 'clickable' : ''}`}
-            onClick={() => {props.assignMinMaxValue(props.min);}}>
+          <p className={props.min > props.amount && props.amount !== '...' ? 'error' : ''}>
             {t('exchangewidget.min')}: {props.min}
           </p>
 
-          <p className={`${props.max < props.amount && props.amount !== '...' ? 'error' : ''} ${clickable ? 'clickable' : ''}`}
-            onClick={() => {props.assignMinMaxValue(props.max);}}>
+          <p className={props.max < props.amount && props.amount !== '...' ? 'error' : ''}>
             {t('exchangewidget.max')}: {props.max}
           </p>
         </div>
