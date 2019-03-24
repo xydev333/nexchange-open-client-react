cc2828ae78ac0c3733988a860ded7f446e341f88
SuperMan0719 <buper0719@gmail.com>
2019-03-24 22:58:00 +0000

    Removed Unnecessary  code

--- src/components/Home/Hero/ExchangeWidget/WalletAddress/WalletAddress.js
+++ src/components/Home/Hero/ExchangeWidget/WalletAddress/WalletAddress.js
@@ -3,24 +3,17 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { errorAlert, setWallet } from 'Actions/index.js';
 import validateWalletAddress from 'Utils/validateWalletAddress';
-import AddressHistory from './AddressHistory/AddressHistory';
 import styles from './WalletAddress.scss';
 import { I18n } from 'react-i18next';
 import i18n from '../../../../../i18n';
-
-import urlParams from 'Utils/urlParams';
-
 
 class WalletAddress extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { address: '', firstLoad: true , addressHistory: []};
+    this.state = { address: '' };
     this.handleChange = this.handleChange.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.setAddress = this.setAddress.bind(this);
   }
 
   validate = (address, receiveCoin) => {
@@ -56,20 +49,6 @@ class WalletAddress extends Component {
     this.validate(address, this.props.selectedCoin.receive);
   }
 
-  handleFocus(event) {
-    const receiveCoin = this.props.selectedCoin.receive;
-    const addressHistory = localStorage[`${receiveCoin}addressHistory`] ? localStorage[`${receiveCoin}addressHistory`].split(",") : [];
-    this.setState({
-      addressHistory
-    });
-  }
-
-  handleBlur(event) {
-    this.setState({
-      addressHistory: []
-    });
-  }
-
   handleSubmit(event) {
     event.preventDefault();
     this.props.onSubmit();
@@ -79,22 +58,6 @@ class WalletAddress extends Component {
     if (nextProps.selectedCoin.receive !== this.props.selectedCoin.receive) {
       this.validate(this.state.address, nextProps.selectedCoin.receive);
     }
-  }
-
-  componentDidUpdate(){
-    //Check if withdraw_address url param exists. If exists, prefill address field with that value
-    const params = urlParams();
-    if (params && params.hasOwnProperty('withdraw_address') && !this.props.wallet.address
-      && this.props.selectedCoin.receive && this.state.firstLoad) {
-        const simulatedEvent ={target: {value: params['withdraw_address'].toString()}};
-        this.handleChange(simulatedEvent);
-        this.setState({firstLoad: false});
-      }
-  }
-
-  setAddress(address) {
-    const simulatedEvent ={target: {value: address}};
-    this.handleChange(simulatedEvent);
   }
 
   render() {
@@ -110,12 +73,9 @@ class WalletAddress extends Component {
                 className={`form-control ${styles.input}`}
                 id="withdraw-addr"
                 onChange={this.handleChange}
-                onFocus={this.handleFocus}
-                onBlur={this.handleBlur}
                 value={this.state.address}
                 placeholder={t('generalterms.youraddress', { selectedCoin: coin })}
               />
-              <AddressHistory history={this.state.addressHistory} setAddress={this.setAddress} />
             </form>
           </div>
         )}
