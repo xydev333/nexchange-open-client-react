39e18c6fcac3b3a3b0d36346514de1cb76dfdec5
SuperMan0719 <buper0719@gmail.com>
2019-05-03 12:17:42 +0100

    fix

--- src/components/Home/Hero/ExchangeWidget/WalletAddress/WalletAddress.js
+++ src/components/Home/Hero/ExchangeWidget/WalletAddress/WalletAddress.js
@@ -128,7 +128,7 @@ class WalletAddress extends Component {
 
   UNSAFE_componentWillReceiveProps(nextProps) {
     if (nextProps.selectedCoin[this.props.withdraw_coin] !== this.props.selectedCoin[this.props.withdraw_coin]) {
-      this.validate(this.props.wallet.address, nextProps.selectedCoin[this.props.withdraw_coin]);
+      this.validate(nextProps.wallet.address, nextProps.selectedCoin[this.props.withdraw_coin]);
     }
 
     try {
@@ -209,14 +209,14 @@ class WalletAddress extends Component {
                 autoComplete="off"
                 placeholder={t('generalterms.youraddress', { selectedCoin: coin })}
               />
-              {!_.isEmpty(this.orderHistory)
-                ? <button onClick={(e) => this.setFocus(e)} className={styles.previousAddress}>
-                  <div className="visible-xs visible-sm"><i className="fas fa-history"></i></div>
-                  <div className="visible-md visible-lg">
-                    {this.props.orderMode === 'BASIC' ? t('generalterms.usepreviousaddress') : <i className="fas fa-history"></i>}
-                  </div>
-                </button>
-                : null}
+              {!_.isEmpty(this.orderHistory) 
+               ?  <button onClick={(e) => this.setFocus(e)} className={styles.previousAddress}>
+                    <div className="visible-xs visible-sm"><i className="fas fa-history"></i></div>
+                    <div className="visible-md visible-lg">
+                      {this.props.orderMode != 'ORDER_BOOK' ? t('generalterms.usepreviousaddress') : <i className="fas fa-history"></i>}
+                    </div>
+                  </button>
+               :  null}
               {this.state.showHistory ?
                 <AddressHistory
                   history={this.orderHistory}
