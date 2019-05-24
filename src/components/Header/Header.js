963d798b04562d365411c3ab0e1f484c91f8ba9d
SuperMan0719 <buper0719@gmail.com>
2019-05-24 16:04:27 +0100

    Added Limit Order Cypress test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -52,26 +52,13 @@ class Header extends Component {
   }
 
   isHomeHeader = () => {
-    if (window.location.pathname === '/' 
-        || window.location.pathname.indexOf('/faqs') != -1
-        || window.location.pathname === '/not-found') {
-      return true;
-    }
-    return false;
-  }
-
-  hideHeader = () => {
-    if (window.location.pathname === '/signin' 
-        || window.location.pathname === '/signup'
-        || window.location.pathname === '/forgot-password') {
+    if (window.location.pathname === '/' || window.location.pathname.indexOf('/faqs') != -1) {
       return true;
     }
     return false;
   }
 
   render() {
-    const hideHeader = this.hideHeader();
-    if(hideHeader) return null;
     const isHomeHeader = this.isHomeHeader();
     return (
       <I18n ns="translations">
@@ -113,7 +100,7 @@ class Header extends Component {
 
                   <li>
                     <a
-                      className={`${styles.link} hidden-sm hidden-md`}
+                      className={`${styles.link} hidden-sm`}
                       href="http://docs.nexchange2.apiary.io/"
                       target="_blank"
                       rel="noopener noreferrer"
@@ -124,11 +111,11 @@ class Header extends Component {
                     </a>
                   </li>
 
-                  {/* <li>
-                    <Link onClick={() => this.closeNavbar()} to="/#compare" className={`${styles.link} hidden-sm`} data-test="compare-link">
+                  <li>
+                    <Link onClick={() => this.closeNavbar()} to="/#compare" className={styles.link} data-test="compare-link">
                         {t('header.compare')}
                     </Link>
-                  </li> */}
+                  </li>
 
                   <li>
                     <Link 
@@ -137,17 +124,6 @@ class Header extends Component {
                       to='#'
                       data-test="support-btn">
                         {t('header.support')}
-                    </Link>
-                  </li>
-
-                  <li>
-                   <Link onClick={() => this.closeNavbar()} to="/signin" className={styles.link}>
-                        {t('accounts.signin')}
-                    </Link>
-                  </li>
-                  <li>
-                   <Link onClick={() => this.closeNavbar()} to="/signup" className={`${styles.link} ${styles.main}`}>
-                        {t('accounts.signup')}
                     </Link>
                   </li>
 
