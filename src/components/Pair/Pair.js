740f91c19b4b2fd3e2f826a9c1fc71e53dedc309
SuperMan0719 <buper0719@gmail.com>
2019-09-30 14:23:39 +0100

    Bumped react-router-dom from 4.2.2 to 5.1.1

---
+++ src/components/Pair/Pair.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { fetchCoinDetails, fetchPairs, changeOrderMode } from 'Actions';
+import Hero from './Hero/Hero';
+import Articles from './Articles/Articles';
+import PriceChart from './PriceChart/PriceChart';
+import RecentOrders from './RecentOrders/RecentOrders';
+
+export class Pair extends Component {
+  componentDidMount() {
+    this.props.fetchCoinDetails();
+    this.props.fetchPairs();
+  }
+  componentDidUpdate(prevProps, prevState) {
+    // Detect coin change by link
+    const oldUrlParams = new URLSearchParams(prevProps.location.search);
+    const oldPairParam = oldUrlParams.get('pair');
+    const newUrlParams = new URLSearchParams(this.props.location.search);
+    const newPairParam = newUrlParams.get('pair');
+    if(newPairParam && newPairParam !== oldPairParam) {
+        this.props.fetchCoinDetails();
+        this.props.fetchPairs();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Hero {...this.props} />
+        <PriceChart />
+        <RecentOrders />
+        <Articles />
+        {/* TODO Referral Program Widget */}
+        {/* TODO API Access Widget */}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ orderMode, coinsInfo, selectedCoin }) => ({ orderMode, coinsInfo, selectedCoin });
+const mapDispatchToProps = dispatch => bindActionCreators({ fetchCoinDetails, fetchPairs, changeOrderMode }, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Pair);
