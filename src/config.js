173d6c5532a0e1de6fd7466d847decdb15492309
SuperMan0719 <buper0719@gmail.com>
2019-12-03 13:12:50 +0200

    fix hooks after return value

--- src/config.js
+++ src/config.js
@@ -13,8 +13,7 @@ const config = {
   KYC_DETAILS_FETCH_INTERVAL: 20000,
   ADVANCED_MODE_ENABLED: true,
   GRAPHCMS_API: 'https://api-euwest.graphcms.com/v1/ck2olkts50bo501i0h3ej7d1f/master',
-  INTERCOM_APP_ID: 'a3zrft9d',
-  AUTH_CLIENT_ID: 'bW59HEU7snRQfu9dcToFVSBuwLQ5du4JbrxenYkS'
+  INTERCOM_APP_ID: 'a3zrft9d'
 };
 
 export default config;
