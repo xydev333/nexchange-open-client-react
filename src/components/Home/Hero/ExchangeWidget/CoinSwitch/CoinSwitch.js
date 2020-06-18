c8f2f3cfcd184d3eec147803bd9a4c4646c1ee86
SuperMan0719 <buper0719@gmail.com>
2020-06-18 18:51:16 +0530

    replace bdg with bch in coinprices

--- src/components/Home/Hero/ExchangeWidget/CoinSwitch/CoinSwitch.js
+++ src/components/Home/Hero/ExchangeWidget/CoinSwitch/CoinSwitch.js
@@ -7,33 +7,46 @@ import styles from './CoinSwitch.scss';
 class CoinSwitch extends Component {
   state = {};
 
-  componentDidMount() {}
+  componentDidMount() {
+  }
 
-  handleClick = () => {
+  switchCoins = () => {
     const deposit = this.props.selectedCoin.receive;
     const receive = this.props.selectedCoin.deposit;
 
-    this.props.selectCoin(
-      {
-        ...this.props.selectedCoin,
-        deposit,
-        receive,
-        selectedByUser: {
-          deposit: true,
-          receive: true,
-        },
-        orderByAddress: false,
+    this.props.selectCoin({
+      ...this.props.selectedCoin,
+      deposit,
+      receive,
+      selectedByUser: {
+        deposit: true,
+        receive: true
       },
-      this.props.pairs
-    );
+      orderByAddress: false,
+    });
 
-    window.gtag('event', 'Switched coins', { event_category: 'Order', event_label: `${deposit} - ${receive}` });
+    window.gtag('event', 'Switched coins', {event_category: 'Order', event_label: `${deposit} - ${receive}`});
+  };
+
+  handleClick = () => {
+    this.switchCoins();
   };
 
   render() {
+    let switchButtonEnabled = false;
+    const nextDeposit = this.props.selectedCoin.receive;
+    const nextReceive = this.props.selectedCoin.deposit;
+
+    if(this.props.pairs && this.props.pairs[nextDeposit] && this.props.pairs[nextDeposit][nextReceive]){
+      switchButtonEnabled = true;
+    }
+
     return (
       <div className={`col-xs-12 col-sm-1 ${styles.container}`}>
-        <span className={`${styles.icon}`} onClick={this.handleClick}></span>
+      <span 
+        className={`${styles.icon} ${!switchButtonEnabled ? styles['icon-disabled'] : ''}`} 
+        onClick={switchButtonEnabled ? this.handleClick : null}>
+      </span>
       </div>
     );
   }
@@ -42,4 +55,7 @@ class CoinSwitch extends Component {
 const mapStateToProps = ({ selectedCoin, pairs }) => ({ selectedCoin, pairs });
 const mapDispatchToProps = dispatch => bindActionCreators({ selectCoin }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoinSwitch);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoinSwitch);
