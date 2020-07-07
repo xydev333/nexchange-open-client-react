0eda70aa12ab49b2f047db76f18db94b707e6c2b
SuperMan0719 <buper0719@gmail.com>
2020-07-07 14:42:59 +0600

    Add files via upload

--- src/components/Home/Hero/Integrations/Integrations.js
+++ src/components/Home/Hero/Integrations/Integrations.js
@@ -4,17 +4,19 @@ import Metamask from './Metamask/Metamask';
 
 import styles from './Integrations.scss';
 
-const Integrations = () => (
-  <div className={styles.container}>
-    <div className={styles['separator-container']}>
-      <div className={styles.separator}></div>
-      <span>Or connect with</span>
-      <div className={styles.separator}></div>
-    </div>
-    <div className={styles.integrations}>
-      <Metamask />
-    </div>
-  </div>
-);
+const Integrations = props => {
+    return (<div className={styles.container}>
+                <div className={styles['separator-container']}>
+                    <div className={styles.separator}></div>
+                    <span>Or connect with</span>
+                    <div className={styles.separator}></div>
+                </div>
+                <div className={styles.integrations}>
+                    <Metamask />
+                </div>
+            </div>)
+}
+
+
 
 export default Integrations;
